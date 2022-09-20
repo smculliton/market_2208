18d628948b987e6a98fc24cb066116f3250fc970
Sean Culliton <smculliton@gmail.com>
2022-09-20 09:09:49 -0600

    Finish item class

---
+++ spec/item_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/item' 
+
+RSpec.describe Item do 
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+  describe '#initialize' do 
+    it 'exists' do 
+      expect(@item1).to be_a Item
+    end
+    it 'has a name' do 
+      expect(@item1.name).to eq('Peach')
+    end
+    it 'has a price' do 
+      expect(@item1.price).to eq('$0.75')
+    end
+  end
+end
---
+++ spec/vendor_spec.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+require './lib/vendor'
+
+RSpec.describe Vendor do 
+  describe '#initialize' do 
+    it ''
